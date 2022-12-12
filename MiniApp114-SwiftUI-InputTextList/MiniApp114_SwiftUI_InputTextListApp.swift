c1dd1ca91949e0dc8d6a7d8a618e5cf29ca2072b
Kota <maedakoutaa@gmail.com>
2022-12-12 10:52:52 +0900

    Initial Commit

---
+++ MiniApp114-SwiftUI-InputTextList/MiniApp114_SwiftUI_InputTextListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp114_SwiftUI_InputTextListApp.swift
+//  MiniApp114-SwiftUI-InputTextList
+//
+//  Created by 前田航汰 on 2022/12/12.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp114_SwiftUI_InputTextListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
