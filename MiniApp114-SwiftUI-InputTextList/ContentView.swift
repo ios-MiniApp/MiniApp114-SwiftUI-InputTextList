32e67dc08719128202dd2cb2b05b0613e7823b06
Kota <maedakoutaa@gmail.com>
2022-12-13 09:01:23 +0900

    commit

--- MiniApp114-SwiftUI-InputTextList/ContentView.swift
+++ MiniApp114-SwiftUI-InputTextList/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    let fruits = ["リスト１", "リスト２", "リスト３"]
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            List {
+                ForEach(0 ..< fruits.count) { index in
+                    Text(fruits[index])
+                }
+            }
         }
-        .padding()
     }
 }
 
